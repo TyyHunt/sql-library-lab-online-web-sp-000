6dece082edd95bc2c80146baacf14c7760814110
Tyler Hunt <tyhunt04@gmail.com>
2019-03-16 15:33:23 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,7 +15,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgeres.name FROM series
+  "SELECT authors.name, subgenres.name FROM series
    INNER JOIN authors on authors.id = series.author_id
    INNER JOIN subgenres on subgenres.id = series.subgenre_id
    ORDER BY authors.name;"
