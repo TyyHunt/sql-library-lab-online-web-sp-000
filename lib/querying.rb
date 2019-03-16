d3e96865db65c737d06bbc4485d660fab3f3b62d
Tyler Hunt <tyhunt04@gmail.com>
2019-03-16 15:17:04 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,7 +18,7 @@ def select_name_and_series_subgenres_of_authors
   "SELECT authors.name and subgenres.name FROM series
    INNER JOIN authors on authors.id = series.author_id
    INNER JOIN subgenres on subgenres.id = series.subgenre_id
-   ;"
+   ORDER BY authors.name;"
 end
 
 def select_series_title_with_most_human_characters
