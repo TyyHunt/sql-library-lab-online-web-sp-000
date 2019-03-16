291994142001234ec9d53e9d31f13fe0220482e2
Tyler Hunt <tyhunt04@gmail.com>
2019-03-16 15:16:17 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,7 +17,7 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name and subgenres.name FROM series
    INNER JOIN authors on author.id = series.author_id
-   INNER JOIN subgenres on subgenre.id = series.subgenre_id 
+   INNER JOIN subgenres on subgenre.id = series.subgenre_id
    ;"
 end
 
